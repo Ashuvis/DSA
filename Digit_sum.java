899a0544ee8f80b422f530f31b44ca3f7208b50e
Ashuvis <ashutoshvishwakarma2478@gmail.com>
2026-07-05 08:06:09 +0530

    sum of digit using recursion

--- Digit_sum.java
+++ Digit_sum.java
@@ -1,3 +1,13 @@
 public class Digit_sum {
-    
+    public static void main(String[] args) {
+        int ans=sum(123456);
+        System.out.println(ans);
+    }
+    static int sum(int n) {
+        if (n == 0) {
+            return 0;
+        } else {
+            return n % 10 + sum(n / 10);
+        }
+    }
 }
